0171bbdc4c43eed3e2d6f50b7a4430188a0b639a
Finnbar Keating <finnjkeating@gmail.com>
2021-10-14 12:00:58 +0100

    Whoops, broke Main

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,7 @@ list = 3 :+: True :+: () :+: HNil
 -- we have to specify the type directly with type annotations (even though
 -- the types of list and list' should fully infer it)
 list' :: HList '[Bool, (), Int]
-list' = $$(rearrange @'[Int, Bool, ()] @'[Bool, (), Int]) list
+list' = $$(rearr @'[Int, Bool, ()] @'[Bool, (), Int]) list
 
 main :: IO ()
 main = print list'
