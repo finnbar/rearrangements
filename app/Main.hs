dfff82405936b16165d29f5e3d35b34b83712456
Finnbar Keating <finnjkeating@gmail.com>
2021-10-15 15:24:35 +0100

    swapped list' and list'' for explanation

--- app/Main.hs
+++ app/Main.hs
@@ -10,14 +10,14 @@ import Data.HList
 list :: HList '[Int, Bool, ()]
 list = 3 :+: True :+: () :+: HNil
 
+list' :: HList '[Bool, (), Int]
+list' = rearrange list
+
 -- due to the discussion in https://gitlab.haskell.org/ghc/ghc/-/issues/10271,
 -- we have to specify the type directly with type annotations (even though
 -- the types of list and list' should fully infer it)
-list' :: HList '[Bool, (), Int]
-list' = $$(rearrangeTH @'[Int, Bool, ()] @'[Bool, (), Int]) list
-
 list'' :: HList '[Bool, (), Int]
-list'' = rearrange list
+list'' = $$(rearrangeTH @'[Int, Bool, ()] @'[Bool, (), Int]) list
 
 main :: IO ()
 main = print list' >> print list''
