e22c9304d4274cc6346db816aab3933b37600157
Finnbar Keating <finnjkeating@gmail.com>
2021-10-19 16:39:13 +0100

    first pass at flatten

--- src/Rearrange/Treearrange.hs
+++ src/Rearrange/Treearrange.hs
@@ -15,48 +15,42 @@ import Data.HList
 -- (i.e. we essentially do rearr l = getFirstElem :+: rearr l.)
 -- We likely want to stick with this pattern - it's just a kinding challenge.
 
--- TODO: work out how to get the kind involved. Proxy??
-class RearrangeTree a b k where
-    rearrTree :: a -> b
+-- Flattening of nested HLists into a single HList
 
-instance RearrangeTree a (HList '[]) k where
-    rearrTree = const HNil
+{-
+class FlattenNestedHLists k xs (ys :: [k]) | xs -> ys where
+    flattenH :: HList xs -> HList ys
 
-instance (LookupH k x a a', RearrangeTree a (HList xs) k)
-    => RearrangeTree a (HList (x ': xs)) k where
-    rearrTree env = lookupH env $ rearrTree @_ @_ @k env
+instance FlattenNestedHLists k '[] '[] where
+    flattenH = const HNil
 
-instance (RearrangeTree a (HList xs) k, RearrangeTree a (HList ys) k)
-    => RearrangeTree a (HList (HList xs ': ys)) k where
-        rearrTree env = rearrTree @a @(HList xs) @k env
-            :+: rearrTree @a @(HList ys) @k env
+instance {-# OVERLAPPING #-} (zs ~ Append ys' xs', FlattenNestedHLists k xs xs',
+    FlattenNestedHLists k ys ys')
+    => FlattenNestedHLists k (HList ys ': xs) zs where
+        flattenH (list :+: lists) = flattenH @k @ys @ys' list
+            `hAppend` flattenH @k @xs @xs' lists
 
--- TODO: this is blatantly wrong.
-class LookupH k x a a' where
-    lookupH :: a -> (x, a')
+instance {-# OVERLAPPABLE #-} (FlattenNestedHLists k xs ys)
+    => FlattenNestedHLists k (x ': xs) (x ': ys) where
+        flattenH (el :+: list) = el :+: flattenH list
+-}
 
--- Instance for lists which consist of lists
+class FlattenNestedHLists x y | x -> y where
+    flattenH :: x -> y
 
-class LookupHNodes k (x :: *) (xs :: [*]) (xs' :: [*]) | x xs -> xs' where
-    removeHNodes :: HList xs -> (HList ys -> HList (x ': ys), HList xs')
-    getHNodes :: HList xs -> (HList ys -> HList (x ': ys))
+instance FlattenNestedHLists (HList ('[] @k)) (HList ('[] @k)) where
+    flattenH = const HNil
 
--- TODO: this implementation. The challenge, as always, is to find out which
--- way (in a list) to go at compile time.
+instance {-# OVERLAPPING #-} (zs ~ Append ys' xs', FlattenNestedHLists (HList xs) (HList xs'),
+    FlattenNestedHLists (HList ys) (HList ys'))
+    => FlattenNestedHLists (HList (HList ys ': xs)) (HList zs) where
+        flattenH (list :+: lists) = flattenH @(HList ys) @(HList ys') list
+            `hAppend` flattenH @(HList xs) @(HList xs') lists
 
--- Instance for lists of nodes of kind k (no recursion)
+instance {-# OVERLAPPING #-} (FlattenNestedHLists (HList ys) (HList ys'))
+    => FlattenNestedHLists (HList '[HList ys]) (HList ys') where
+        flattenH (list :+: _) = flattenH @(HList ys) @(HList ys') list
 
-class LookupHLeaves k (x :: k) (xs :: [k]) (xs' :: [k]) | x xs -> xs' where
-    removeHLeaves :: HList xs -> (HList ys -> HList (x ': ys), HList xs')
-    getHLeaves :: HList xs -> (HList ys -> HList (x ': ys))
-
-instance LookupHLeaves k x (x ': xs) xs where
-    removeHLeaves (x :+: xs) = ((x :+:), xs)
-    getHLeaves (x :+: _) = (x :+:)
-
-instance (LookupHLeaves k x xs xs') =>
-    LookupHLeaves k x (y ': xs) (y ': xs') where
-        removeHLeaves (x :+: xs) =
-            let (res, rest) = removeHLeaves xs
-            in (res, x :+: rest)
-        getHLeaves (_ :+: xs) = getHLeaves xs
+instance {-# OVERLAPPABLE #-} (FlattenNestedHLists (HList xs) (HList ys))
+    => FlattenNestedHLists (HList (x ': xs)) (HList (x ': ys)) where
+        flattenH (el :+: list) = el :+: flattenH list
